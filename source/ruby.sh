d30edf40da02be093acd268701acd902f68d14e2
Philipp Koster <philipp.koster@startmail.com>
2016-10-27 12:47:52 +0200

    switched to rbenv

--- source/ruby.sh
+++ source/ruby.sh
@@ -1,6 +1,1 @@
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-
-
 alias be='bundle exec'
